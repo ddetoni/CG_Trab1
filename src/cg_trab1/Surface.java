e98ad81bed4170db247ab155a774fd3fead6ec3c
unknown <lucianogluz@gmail.com>
2013-11-13 02:34:30 -0200

    Quadrado

--- src/cg_trab1/Surface.java
+++ src/cg_trab1/Surface.java
@@ -1,8 +1,12 @@
 package cg_trab1;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -13,7 +17,9 @@ public class Surface extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private static final int sleepTime = 5;
+	private static final int sleepTime = 20;
+	private static final int step = 60;
+	private static final int size = 50;
 	
 	/**
 	 * @param args
@@ -22,7 +28,7 @@ public class Surface extends JFrame {
 		
 		Surface s = new Surface();
 		s.setTitle("Circle");
-		s.setSize(500, 500);
+		s.setSize(900, 600);
 		s.setVisible(true);
 
 	}
@@ -32,10 +38,10 @@ public class Surface extends JFrame {
 		ArrayList<Point> infinitePoints = new ArrayList<Point>();
 		
 		Circle circleE = new Circle();
-		circleE.midPointCircle(150, 250, 50);
+		circleE.midPointCircle(250, 280, 200);
 		
 		Circle circleD = new Circle();
-		circleD.midPointCircle(250, 250, 50);
+		circleD.midPointCircle(650, 280, 200);
 		
 		infinitePoints.addAll(circleE.getOctant(1, -1));
 		infinitePoints.addAll(circleE.getOctant(5, 1));
@@ -66,20 +72,58 @@ public class Surface extends JFrame {
 		ArrayList<Point> infPoints = this.calculateInfiniteSymbolPoints();
 		
 		Graphics2D g2d = (Graphics2D) g;
-		
-		for(int i=0; i < infPoints.size(); i++)
-		{
-			Point point = infPoints.get(i);
-			g2d.drawLine(point.x, point.y, point.x, point.y);
+		int j = 0;
+		int count = 0;
+		boolean aux = false;
+		while(j<infPoints.size()&&!aux){
+			for(int i=0; i < infPoints.size(); i++)
+			{
+				Point point = infPoints.get(i);
+				g2d.drawLine(point.x, point.y, point.x, point.y);
+				/*
+				try {
+					Thread.sleep(sleepTime);
+				} catch (InterruptedException e) {
+					System.out.printf("Erro no Sleep.");
+					e.printStackTrace();
+				}*/
+			}
+			int auxStep = step;
+			Point point;
+			if(j*step>infPoints.size()){
+				point = infPoints.get(infPoints.size()-1);
+				aux = true;
+			}else{
+				point = infPoints.get(j*step);
+			}
+			
+			Shape rec ;
+			AffineTransform affine = new AffineTransform();
+			if(count==0){
+				rec = new Rectangle(point.x-((size-15)/2),point.y-((size-15)/2),size-15,size-15);
+				affine.setToRotation(45,point.x,point.y);
+				affine.rotate(45,point.x,point.y);
+				count++;
+			}else{
+				rec = new Rectangle(point.x-(size/2),point.y-(size/2),size,size);
+				affine.setToRotation(-45,point.x,point.y);
+				affine.rotate(-45,point.x,point.y);
+				count--;
+			}
+			if(j==0){
+				g2d.draw(rec);
+			}else{
+				rec =  affine.createTransformedShape(rec);
+				g2d.draw(rec);
+			}
 			
 			try {
-				Thread.sleep(sleepTime);
+				Thread.sleep(sleepTime+100);
 			} catch (InterruptedException e) {
-				System.out.printf("Erro no Sleep.");
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			j++;
+			clearWindow(g2d);
 		}
-		
 	}
-
-}
